3c67c8c68d57a7dd002e8e7e661ce5dd709c5b91
bryokn <bryokn@gmail.com>
2024-04-09 00:33:18 +0300

    updates

--- server/app.py
+++ server/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-from models import db, Message
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+from models import db, Message
 
 CORS(app)
 migrate = Migrate(app, db)
@@ -23,4 +24,48 @@ def messages_by_id(id):
     return ''
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000)
+
+'''
+@app.route('/messages', methods=['GET'])
+def messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<intdb.init_app(app)
+
+@app.route('/messages')
+def messages():
+    return ''
+
+@app.route('/messages/<int:id>')
+def messages_by_id(id):
+    return ''
+
+if __name__ == '__main__':
+    app.run(port=5555):id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted'}), 204
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5555)'''
